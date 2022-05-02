ae0644ca6294e9e79af0143c549969ad13324d32
AndrewDGordon <14862629+AndrewDGordon@users.noreply.github.com>
2022-05-02 18:21:33 +0100

    some outputs

--- process.py
+++ process.py
@@ -2,11 +2,21 @@
 
 import json
 
-my_string = '''
-{
-    "date": 2022,
-    "family": [ "Kate", "Georgia", "Angus"]
-}
-'''
+funder='GB-GOR-PB188'
+with open(funder+'.json') as f:
+    data = json.load(f)
 
-print(my_string)
+#print(data)
+
+recipients = [ item['org_name'] for item in data['data'] ]
+output = {'funder': funder, 'recipients': recipients}
+jsonString = json.dumps(output, indent=2)
+
+# print(jsonString)
+
+file = r'output/'+funder+r'.json'
+print(file)
+
+myText = open(file,'w')
+myText.write(jsonString)
+myText.close()
